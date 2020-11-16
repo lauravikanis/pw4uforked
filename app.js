420627afc6337be042fd4e96f528c639292ed5d7
lmachens <leon.machens@googlemail.com>
2020-11-16 10:49:45 +0100

    Update password if already exist

--- app.js
+++ app.js
@@ -7,31 +7,40 @@ const { askForMasterPassword } = require("./lib/questions");
 const { isMasterPasswordCorrect } = require("./lib/validation");
 
 async function run() {
-  console.log("Connecting to database...");
-  await connect(process.env.MONGO_DB_URI, process.env.MONGO_DB_NAME);
-  console.log("Connected to database 🎉");
+  try {
+    console.log("Connecting to database...");
+    await connect(process.env.MONGO_DB_URI, process.env.MONGO_DB_NAME);
+    console.log("Connected to database 🎉");
 
-  const masterPassword = await askForMasterPassword();
+    const masterPassword = await askForMasterPassword();
 
-  if (!(await isMasterPasswordCorrect(masterPassword))) {
-    console.error("You are not welcome here! 👿 Try again!");
-    return run();
+    if (!(await isMasterPasswordCorrect(masterPassword))) {
+      console.error("You are not welcome here! 👿 Try again!");
+      return run();
+    }
+
+    const [passwordName, newPasswordValue] = readCommandLineArguments();
+    if (!passwordName) {
+      console.error("Missing password name!");
+      return process.exit(9);
+    }
+
+    if (newPasswordValue) {
+      await setPassword(passwordName, newPasswordValue);
+      console.log(`Password ${passwordName} set 🎉`);
+    } else {
+      const passwordValue = await getPassword(passwordName);
+      console.log(`Your password is ${passwordValue} 🎉`);
+    }
+  } catch (error) {
+    if (error.message.match("E11000")) {
+      console.error("DUPLICATE KEY JO!");
+    } else {
+      console.error(error);
+    }
+  } finally {
+    await close();
   }
-
-  const [passwordName, newPasswordValue] = readCommandLineArguments();
-  if (!passwordName) {
-    console.error("Missing password name!");
-    return process.exit(9);
-  }
-
-  if (newPasswordValue) {
-    await setPassword(passwordName, newPasswordValue);
-    console.log(`Password ${passwordName} set 🎉`);
-  } else {
-    const passwordValue = await getPassword(passwordName);
-    console.log(`Your password is ${passwordValue} 🎉`);
-  }
-  await close();
 }
 
 run();
