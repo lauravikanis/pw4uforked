2616b8ef4758fb9ede8a9335c962f1c3514a009d
lauravikanis <lauravikanis@yahoo.de>
2020-11-16 14:28:29 +0100

    add get fuction

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
+require("dotenv").config();
+
 const express = require("express");
+const { getPassword } = require("./lib/passwords");
+const { connect } = require("./lib/database");
+
 const app = express();
 const port = 3600;
 
-app.get("/", (request, response) => {
-  response.send("HeWIFI is ASJKHSDGPFJDS");
+//route zu "passwords/:name"
+
+app.get("/passwords/:name", async (request, response) => {
+  const { name } = request.params;
+  const passwordValue = await getPassword(name);
+  response.send(passwordValue);
 });
 
-app.listen(port, () => {
-  console.log(`Passwordmanager listening at http://localhost:${port}`);
+app.post("/passwords", (request, response) => {
+  response.send("Under construction");
 });
+
+async function run() {
+  console.log("Connecting to database...");
+  await connect(process.env.MONGO_DB_URI, process.env.MONGO_DB_NAME);
+  console.log("Connected to database 🎉");
+
+  app.listen(port, () => {
+    console.log(`PW4U API listening at http://localhost:${port}`);
+  });
+}
+
+run();
