dedda33e4c29d3dbba3b0947ddfff63fc099ccf2
lauravikanis <lauravikanis@yahoo.de>
2020-11-18 16:06:23 +0100

    Stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,47 @@
-import { useEffect, useState } from "react";
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 import { getPassword } from "./api/passwords";
+import useAsync from "./hooks/useAsync";
+import styled from "styled-components/macro";
+
+const Passwordform = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 2.5rem;
+
+  input {
+    margin: 1rem;
+  }
+`;
 
 function App() {
-  const [password, setPassword] = useState(null);
+  const { data, loading, error, doFetch } = useAsync(() =>
+    getPassword(InputValue)
+  );
 
-  useEffect(() => {
-    const doFetch = async () => {
-      const newPassword = await getPassword("wifi");
-      setPassword(newPassword);
-    };
-    doFetch();
-  }, []);
+  const [InputValue, setInputValue] = useState("");
+
   return (
     <div className="App">
-      <prompt when={getPassword} message="Whats the master password?" />
-      <form>
-        <input></input>
-        <input></input>
-        Passwordname: {password}
-      </form>
+      {loading && <div> Loading...</div>}
+      {error && <div>{error.message}</div>}
+      <Passwordform
+        onSubmit={(event) => {
+          event.preventDefault();
+          doFetch();
+          setInputValue("");
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Passwordname?"
+          value={InputValue}
+          onChange={(event) => setInputValue(event.target.value)}
+          required
+        />
+        <div>Your password is: {data}</div>
+      </Passwordform>
     </div>
   );
 }
